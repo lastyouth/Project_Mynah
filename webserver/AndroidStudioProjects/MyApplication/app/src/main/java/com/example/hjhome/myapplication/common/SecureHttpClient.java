8a79bfd6b8f5bd74188d08cec925efd8036bba07
HeeJoong Kim <dominyellow@gmail.com>
2015-05-09 00:47:06 +0900

    webserver, android test app upload

---
+++ webserver/AndroidStudioProjects/MyApplication/app/src/main/java/com/example/hjhome/myapplication/common/SecureHttpClient.java
@@ -0,0 +1,9 @@
+package com.example.hjhome.myapplication.common;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * Created by HJHOME on 2015-05-04.
+ */
+public class SecureHttpClient extends DefaultHttpClient{
+}
